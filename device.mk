1bb7eac67e9133c60a511eea2e9816a761dcff29
Emmanuel <alzadidine33@gmail.com>
2022-11-04 15:58:40 +0000

    camellia: Import blobs * Import `first_stage_ramdisk` * Import `MediaTek Bootctrl Library` * Import decryption blob * Import device vintf, firmware
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+# Install gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # A/B
